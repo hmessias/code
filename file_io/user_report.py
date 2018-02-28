78caec339991051b579468c7685a45c7491d6788
hmessias <hmessias@my.bcit.ca>
2018-02-28 12:47:47 -0800

    user_report - final result

--- file_io/user_report.py
+++ file_io/user_report.py
@@ -2,6 +2,7 @@
 
 import os
 
+
 pwd_file = open("/etc/passwd") #read the file /etc/passwd
 
 pwd_file_line = pwd_file.readlines() #read the file one line at a time
@@ -19,11 +20,11 @@ def get_user_accounts(file_list): #gets the user we want from the /etc/passwf fi
 
 def parse_group(group_path='/etc/group'): #gets the group info from the /etc/group file
     group_data = []
-    with open(group_path) as group_file: #read the file /etc/group
+    with open(group_path) as group_file: #read the file /etc/group and assigns its value to group_file
         group_file_strings = group_file.readlines() #reads the file on line at a time and assigns the value to group_file_string
-        for group_line in group_file_strings: #loops the 
+        for group_line in group_file_strings: #loops the groups over the whole list of groups
             group_line = group_line.rstrip()
-            group_data.append(group_line.split(':'))
+            group_data.append(group_line.split(':')) #creates a list of the groups
     return group_data
 
 def get_sup_groups(username, group_data): #this will loop the users over the group list
@@ -36,9 +37,35 @@ def get_sup_groups(username, group_data): #this will loop the users over the gro
         user.append(sup_groups) #appends sup group info to each user 
     return username
 
+def gen_user_report(users_sup_groups, output_file='user_report.txt'):
 
+    file_dir = os.path.dirname(__file__)
+    file_path = os.path.join(file_dir, 'user_report.txt') #uses the current directory
+    over_write_file = open(file_path, 'w')
 
-group_data = parse_group()
-user_accounts = get_user_accounts(pwd_file_line)
-users_sup_groups = get_sup_groups(user_accounts, group_data)
-print(users_sup_groups)
+    for user in users_sup_groups:
+
+        text = """
+        Account Name: {}
+        UID: {}
+        GID: {}
+        Home Dir: {}
+        Shell: {}
+        Supplimentary Groups: {}
+        """.format(user[0], user[2], user[3], user[5], user[6], user[7])
+
+        over_write_file.write(text)
+
+    over_write_file.close()
+
+    
+def main():
+
+    group_data = parse_group()
+    user_accounts = get_user_accounts(pwd_file_line)
+    users_sup_groups = get_sup_groups(user_accounts, group_data)
+    print(users_sup_groups)
+    gen_user_report(users_sup_groups)
+
+if __name__ == '__main__':
+    main()
