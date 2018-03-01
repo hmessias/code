cc4ed852aa5dc08f725918100de1ed705930d865
hmessias <hmessias@my.bcit.ca>
2018-03-01 11:16:13 -0800

    user_report - final comments

--- file_io/user_report.py
+++ file_io/user_report.py
@@ -41,9 +41,9 @@ def gen_user_report(users_sup_groups, output_file='user_report.txt'):
 
     file_dir = os.path.dirname(__file__)
     file_path = os.path.join(file_dir, 'user_report.txt') #uses the current directory
-    over_write_file = open(file_path, 'w')
+    over_write_file = open(file_path, 'w') #opens the file with write privilege
 
-    for user in users_sup_groups:
+    for user in users_sup_groups: #will loop over the users, lines, in users_sup_groups
 
         text = """
         Account Name: {}
@@ -52,14 +52,14 @@ def gen_user_report(users_sup_groups, output_file='user_report.txt'):
         Home Dir: {}
         Shell: {}
         Supplimentary Groups: {}
-        """.format(user[0], user[2], user[3], user[5], user[6], user[7])
+        """.format(user[0], user[2], user[3], user[5], user[6], user[7]) #gets in each line from users_sup_groups, the information to be added on each {}
 
-        over_write_file.write(text)
+        over_write_file.write(text) #after each loop, it writes the file, add the information about the user
 
-    over_write_file.close()
+    over_write_file.close() #it closes the file after all the users be checked
 
     
-def main():
+def main(): #this makes all these functions bellow run when the python file is invoked
 
     group_data = parse_group()
     user_accounts = get_user_accounts(pwd_file_line)
@@ -67,5 +67,5 @@ def main():
     print(users_sup_groups)
     gen_user_report(users_sup_groups)
 
-if __name__ == '__main__':
+if __name__ == '__main__': #this makes the python file, program, runs by itself, when called, invoked
     main()
