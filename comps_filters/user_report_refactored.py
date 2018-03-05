593b815857e81167c22824cf01b491753388e225
hmessias <hmessias@my.bcit.ca>
2018-03-05 14:15:08 -0800

    user_report_refactored - start task

---
+++ comps_filters/user_report_refactored.py
@@ -0,0 +1,16 @@
+
+
+def split_group_line(line)
+
+def parse_groups(file_path='/etc/group'):
+
+def is_user_account(user_data)
+
+def get_user_accounts(passwd_file='/etc/passwd')
+
+def get_sup_groups(user_data, groups)
+
+def gen_user_report(user_account_list, output_file='user_report.txt'):
+
+if __name__ == '__main__': #this makes the python file, program, runs by itself, when called, invoked
+    main()
