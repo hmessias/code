72a75cc026370b2d5c1df47ca2b3a385fe026269
hmessias <hmessias@my.bcit.ca>
2018-03-14 11:26:47 -0700

    uptade

--- docstrings/passwd_analyzer_w_docstrings.py
+++ docstrings/passwd_analyzer_w_docstrings.py
@@ -1,21 +1,22 @@
-
 #! /usr/bin/env python3
 
 import os
+
 
 pwd_file = open("/etc/passwd") #read the file /etc/passwd
 
 pwd_file_line = pwd_file.readlines() #read the file one line at a time
 
-def get_user(pwd_file_line): #gets the user we want from the /etc/passwf file
-    
-    pwd_file_line = pwd_file_line.rstrip() #removes all empty spaces
-    passwd_fields = pwd_file_line.split(':') #splits into lines using the : as reference
-    if int(passwd_fields[2]) >= 1000 or int(passwd_fields[2]) == 0: #gets the user with user id of 0 and bigger than 1000
-        return True
-    else:
-        return False
 
+def get_user_accounts(file_list): #gets the user we want from the /etc/passwf file
+    user_accounts = [] #creates an empty user_accounts list to be filled with the info we want
+    
+    for passwd_line in file_list:
+        passwd_line = passwd_line.rstrip() #removes all empty spaces
+        passwd_fields = passwd_line.split(':') #splits into lines using the : as reference
+        if int(passwd_fields[2]) >= 1000 or int(passwd_fields[2]) == 0: #gets the user with user id of 0 and bigger than 1000
+            user_accounts.append(passwd_fields) #appends this information to the user_accounts list
+    return user_accounts
 
 def parse_group(group_path='/etc/group'): #gets the group info from the /etc/group file
     group_data = []
@@ -36,10 +37,10 @@ def get_sup_groups(username, group_data): #this will loop the users over the gro
         user.append(sup_groups) #appends sup group info to each user 
     return username
 
-def gen_user_report(users_sup_groups, output_file='user_report_refactored.txt'):
+def gen_user_report(users_sup_groups, output_file='user_report.txt'):
 
     file_dir = os.path.dirname(__file__)
-    file_path = os.path.join(file_dir, 'user_report_refactored.txt') #uses the current directory
+    file_path = os.path.join(file_dir, 'user_report.txt') #uses the current directory
     over_write_file = open(file_path, 'w') #opens the file with write privilege
 
     for user in users_sup_groups: #will loop over the users, lines, in users_sup_groups
@@ -57,13 +58,14 @@ def gen_user_report(users_sup_groups, output_file='user_report_refactored.txt'):
 
     over_write_file.close() #it closes the file after all the users be checked
 
+    
 def main(): #this makes all these functions bellow run when the python file is invoked
-   
-    user_accounts = list(filter(get_user, pwd_file_line))
+
     group_data = parse_group()
+    user_accounts = get_user_accounts(pwd_file_line)
     users_sup_groups = get_sup_groups(user_accounts, group_data)
+    print(users_sup_groups)
     gen_user_report(users_sup_groups)
 
 if __name__ == '__main__': #this makes the python file, program, runs by itself, when called, invoked
     main()
-
